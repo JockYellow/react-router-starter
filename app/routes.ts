7442cb378ea864c31652c49859598496d7097447
JockYellow <you@example.com>
2025-10-31 14:41:33 +0800

    about  edit  test

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes"; // 1. 匯入 route
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("about", "routes/about.tsx") // 2. 加入這行來定義 /about 路由
+] satisfies RouteConfig;
