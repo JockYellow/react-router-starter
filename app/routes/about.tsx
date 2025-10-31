053c7208acbd8c222479badfb5c515603cdd9961
JockYellow <you@example.com>
2025-10-31 14:51:06 +0800

    about  edit one  line

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,7 +3,8 @@ export default function AboutPage() {
   return (
     <section>
       <h1>關於我</h1>
-      <p>我從 HTML/CSS 入門，現在正用 React Router 蓋 SPA！</p>
+      <p>我從 HTML/CSS 入門，現在正用 React Router 蓋 SPA！沒錯!!就是這樣</p>
+      <p>你可以在這裡放關於我的內容。</p>
     </section>
   );
 }
